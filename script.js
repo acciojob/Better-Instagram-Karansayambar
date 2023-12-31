8a9aa142e702ba88f747ce0b0e9c31004b741336
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-31 19:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+let draggedElement = null;
+
+    document.addEventListener('dragstart', function (event) {
+      draggedElement = event.target;
+      event.dataTransfer.setData('text/html', draggedElement.innerHTML);
+    });
+
+    document.addEventListener('dragover', function (event) {
+      event.preventDefault();
+    });
+
+    document.addEventListener('drop', function (event) {
+      event.preventDefault();
+      if (event.target.classList.contains('image')) {
+        const droppedElement = event.target;
+        draggedElement.innerHTML = droppedElement.innerHTML;
+        droppedElement.innerHTML = event.dataTransfer.getData('text/html');
+      }
+    });
