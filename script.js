f29f1554dcebc99c18be2fb0efce14f3e574a2b4
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-22 12:46:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
- let draggedElement;
-    document.addEventListener("dragstart", function (e) {
-        draggedElement = e.target;
-        e.dataTransfer.setDragImage(document.createElement("div"), 0, 0);
-    });
+let draggedElement;
 
-    document.addEventListener("dragover", function (e) {
-        e.preventDefault();
-    });
+document.addEventListener("dragstart", function (e) {
+    draggedElement = e.target;
+    e.dataTransfer.setDragImage(document.createElement("div"), 0, 0);
+});
 
-    document.addEventListener("drop", function (e) {
-        e.preventDefault();
+document.addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
 
-        const dropTarget = e.target.closest(".image");
+document.addEventListener("drop", function (e) {
+    e.preventDefault();
 
-        if (dropTarget && dropTarget !== draggedElement) {
-            const temp = draggedElement.innerHTML;
-			const background = draggedElement.style.backgroundImage;
-            draggedElement.innerHTML = dropTarget.innerHTML;
-            dropTarget.innerHTML = temp;
-			draggedElement.style.backgroundImage = dropTarget.style.backgroundImage;
-			dropTarget.style.backgroundImage = background;
-        }
-    });
+    const dropTarget = e.target.closest(".image");
+
+    if (dropTarget && dropTarget !== draggedElement) {
+        const tempInnerHTML = draggedElement.innerHTML;
+        const tempBackground = draggedElement.style.backgroundImage;
+
+        draggedElement.innerHTML = dropTarget.innerHTML;
+        draggedElement.style.backgroundImage = dropTarget.style.backgroundImage;
+
+        dropTarget.innerHTML = tempInnerHTML;
+        dropTarget.style.backgroundImage = tempBackground;
+    }
+});
